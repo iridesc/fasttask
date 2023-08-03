ab6b6200bd2cc71aea4eb301339874274e90bb02
Irid <lielang6@live.com>
2023-08-03 10:50:33 +0800

    修改了启动方式

--- fasttask/run.sh
+++ fasttask/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-celery multi start w1 -A celery_app -l info
+nohup celery -A celery_app worker --loglevel=info >/var/log/celery.log 2>&1 &
 nohup uvicorn api:app --host 0.0.0.0 --port 80 --reload >/var/log/uvicorn.log 2>&1 &
-tail -f /var/log/uvicorn.log /var/log/celery/*.log
-# python
+tail -f /var/log/celery.log /var/log/uvicorn.log /var/log/celery/*.log
