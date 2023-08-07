5c12d00eb58e462db0d98008219ce148d9a1d01e
Irid <lielang6@live.com>
2023-08-07 20:46:27 +0800

    完善了测试

--- fasttask/tasks/get_hypotenuse.py
+++ fasttask/tasks/get_hypotenuse.py
@@ -1,7 +1,7 @@
 from celery_app import app
 from pydantic import BaseModel
 
-from tasks.packages.tools import xx
+from tasks.packages.tools import xx, sleep_random
 from typing import Union
 
 
@@ -18,6 +18,7 @@ class Result(BaseModel):
 def get_hypotenuse(a, b):
     if a <= 0 or b <= 0:
         raise ValueError("side length must > 0")
-
+    print("running...")
+    sleep_random()
     result = Result(hypotenuse=(xx(a) + xx(b))**0.5)
     return result.model_dump()
