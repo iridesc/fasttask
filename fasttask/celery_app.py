46c6c9df60da37c5265865bb11d6a8086feb4d6a
zhangziyuan <zhangziyuan@nsfocus.com>
2024-05-29 17:06:42 +0800

    修复redis 连接错误

--- fasttask/celery_app.py
+++ fasttask/celery_app.py
@@ -8,8 +8,8 @@ master_passwd = os.environ["master_passwd"]
 
 app = Celery(
     'celery_task',
-    broker=f'redis://{master_passwd}@{master_host}:{master_port}/1',
-    backend=f'redis://{master_passwd}@{master_host}:{master_port}/2',
+    broker=f'redis://:{master_passwd}@{master_host}:{master_port}/1',
+    backend=f'redis://:{master_passwd}@{master_host}:{master_port}/2',
     include=[
         f'loaded_tasks.{task_name}' for task_name in load_task_names("loaded_tasks")
     ],
