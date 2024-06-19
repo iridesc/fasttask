a2a8c582556e3ec27019c11063171a9eaeb7db26
zhangziyuan <zhangziyuan@nsfocus.com>
2024-06-19 15:27:51 +0800

    增加了对任务开始状态的追踪

--- fasttask/celery_app.py
+++ fasttask/celery_app.py
@@ -7,13 +7,14 @@ task_queue_port = os.environ["task_queue_port"]
 task_queue_passwd = os.environ["task_queue_passwd"]
 
 app = Celery(
-    'celery_task',
+    'fasttask',
     broker=f'redis://:{task_queue_passwd}@{master_host}:{task_queue_port}/1',
     backend=f'redis://:{task_queue_passwd}@{master_host}:{task_queue_port}/2',
     include=[
         f'loaded_tasks.{task_name}' for task_name in load_task_names("loaded_tasks")
     ],
-    result_extended=True
+    result_extended=True,
+    task_track_started=True
 )
 
 app.config_from_object("setting", namespace="celery")
