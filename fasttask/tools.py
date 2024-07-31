a54d3ca1c59aafa943138ee8ba4ca207627a0ccd
zhangziyuan <zhangziyuan@nsfocus.com>
2024-07-31 16:56:39 +0800

    增加了统一的环境变量获取

--- fasttask/tools.py
+++ fasttask/tools.py
@@ -2,29 +2,33 @@ import os
 import shutil
 
 
-def strip_task_names(task_names_str):
-    return {task_name.strip() for task_name in task_names_str.split(",") if task_name.strip()}
+def get_bool_env(name):
+    return os.environ.get(name, "False") == "True"
 
 
-def load_task_names(folder_path):
-    enabled_tasks = strip_task_names(os.environ.get("enabled_tasks", ""))
-    disabled_tasks = strip_task_names(os.environ.get("disabled_tasks", ""))
+def get_int_env(name, default=0):
+    return int(os.environ.get(name, default))
 
-    print(f"enabled_tasks={enabled_tasks}")
-    print(f"disabled_tasks={disabled_tasks}")
 
-    need_load_task_names = set()
+def get_list_env(name):
+    return [s.strip() for s in os.environ.get(name, "").split(",") if s.strip()]
+
+
+def load_task_names(folder_path, use_filter=True):
+
+    if use_filter:
+        enabled_tasks = get_list_env("enabled_tasks")
+        disabled_tasks = get_list_env("disabled_tasks")
+
+    task_names = set()
     for task_name in [py_file[:-3] for py_file in os.listdir(folder_path) if py_file.endswith(".py")]:
-        if enabled_tasks and task_name not in enabled_tasks:
-            continue
-        if disabled_tasks and task_name in disabled_tasks:
-            continue
-        need_load_task_names.add(task_name)
-    if not need_load_task_names:
-        raise Exception("no task need load!")
-    print(f"{need_load_task_names=}")
-
-    return need_load_task_names
+        if use_filter:
+            if enabled_tasks and task_name not in enabled_tasks:
+                continue
+            if disabled_tasks and task_name in disabled_tasks:
+                continue
+        task_names.add(task_name)
+    return task_names
 
 
 def rm_tmp_folder(folder_path):
