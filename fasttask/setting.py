e302020118608655bdcf7be17dc58854e0bda10f
zhangziyuan <zhangziyuan@nsfocus.com>
2024-07-31 16:57:34 +0800

    统一了配置的获取

--- fasttask/setting.py
+++ fasttask/setting.py
@@ -3,19 +3,3 @@ project_title = "⚡Fasttask⚡"
 project_summary = "Fasttask Project"
 project_description = "this is a fasttask project!"
 project_version = "ver: 🐕"
-
-api_docs = True
-api_redoc = True
-
-file_download = True
-file_upload = True
-user_to_passwd = {
-    "ZhangSan": "passwd"
-}
-
-
-# celery setting use namespeace "celery"
-# ref: https://docs.celeryq.dev/en/stable/userguide/configuration.html
-# celery_result_expires = 3600
-# celery_worker_pool = "prefork"
-# celery_worker_concurrency = 4
