9372a6435b85ee8aa62c79d9caca813e7bd236c4
zhangziyuan <zhangziyuan@nsfocus.com>
2025-06-11 12:48:48 +0800

    格式化代码

--- fasttask/celery_app.py
+++ fasttask/celery_app.py
@@ -13,34 +13,35 @@ worker_concurrency = get_int_env("worker_concurrency", default=4)
 enabled_task_names = load_task_names("loaded_tasks")
 
 app = Celery(
-    'fasttask',
-    broker=f'redis://:{task_queue_passwd}@{master_host}:{task_queue_port}/1',
-    backend=f'redis://:{task_queue_passwd}@{master_host}:{task_queue_port}/2',
-    include=[
-        f'loaded_tasks.{task_name}' for task_name in enabled_task_names
-    ],
+    "fasttask",
+    broker=f"redis://:{task_queue_passwd}@{master_host}:{task_queue_port}/1",
+    backend=f"redis://:{task_queue_passwd}@{master_host}:{task_queue_port}/2",
+    include=[f"loaded_tasks.{task_name}" for task_name in enabled_task_names],
     result_extended=True,
-    task_track_started=True
+    task_track_started=True,
 )
 
-app.conf.update({
-    'QUEUES': tuple(
-        Queue(task_name, Exchange('tasks_exchange'),
-              routing_key=task_name) for task_name in enabled_task_names
-    ),
-    'DEFAULT_EXCHANGE': 'tasks_exchange',
-    'DEFAULT_EXCHANGE_TYPE': 'direct',
-    'ROUTES': {
-        f'loaded_tasks.{task_name}._{task_name}': {
-            'queue': task_name,
-            'routing_key': task_name,
-        } for task_name in enabled_task_names
-    },
-    'result_expires': result_expires,
-    'worker_pool': worker_pool,
-    'worker_concurrency': worker_concurrency,
-    'broker_connection_retry_on_startup': True,
-})
+app.conf.update(
+    {
+        "QUEUES": tuple(
+            Queue(task_name, Exchange("tasks_exchange"), routing_key=task_name)
+            for task_name in enabled_task_names
+        ),
+        "DEFAULT_EXCHANGE": "tasks_exchange",
+        "DEFAULT_EXCHANGE_TYPE": "direct",
+        "ROUTES": {
+            f"loaded_tasks.{task_name}._{task_name}": {
+                "queue": task_name,
+                "routing_key": task_name,
+            }
+            for task_name in enabled_task_names
+        },
+        "result_expires": result_expires,
+        "worker_pool": worker_pool,
+        "worker_concurrency": worker_concurrency,
+        "broker_connection_retry_on_startup": True,
+    }
+)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.start()
