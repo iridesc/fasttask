98d332525151c5d49f20f0acf70ec2c0e5365fa9
gzzzzxxxx <1733606823@qq.com>
2025-06-26 20:30:16 +0800

    修改初始化脚本逻辑并修复启动异常

--- desktop-init.sh
+++ desktop-init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#更改了可能存在的自动执行顺序错误
-nohup bash -c 'sleep 30 && xfce4-terminal -x bash -c "cd /fasttask && python3 run.py; bash"' > /var/log/desktop_init.log 2>&1 &
+
+nohup xfce4-terminal -x bash -c "sleep 30; cd /fasttask && python3 run.py; bash" > /var/log/desktop_init.log 2>&1 &
