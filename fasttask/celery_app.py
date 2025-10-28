92832164fe497b0afc4f29ce3a2328c06fc26e8f
zhangziyuan <zhangziyuan@nsfocus.com>
2025-10-28 15:52:18 +0800

    优化了载入任务的获取

--- fasttask/celery_app.py
+++ fasttask/celery_app.py
@@ -1,19 +1,20 @@
 import os
 from celery import Celery
+from utils.tools import get_list_env
 from kombu import Queue, Exchange
-from tools import load_task_names
 
 MASTER_HOST = os.environ["MASTER_HOST"]
 TASK_QUEUE_PORT = os.environ["TASK_QUEUE_PORT"]
 TASK_QUEUE_PASSWD = os.environ["TASK_QUEUE_PASSWD"]
 
-enabled_task_names = load_task_names("loaded_tasks")
+
+loaded_tasks = get_list_env("LOADED_TASKS")
 
 app = Celery(
     "fasttask",
     broker=f"redis://:{TASK_QUEUE_PASSWD}@{MASTER_HOST}:{TASK_QUEUE_PORT}/1",
     backend=f"redis://:{TASK_QUEUE_PASSWD}@{MASTER_HOST}:{TASK_QUEUE_PORT}/2",
-    include=[f"loaded_tasks.{task_name}" for task_name in enabled_task_names],
+    include=[f"loaded_tasks.{task}" for task in loaded_tasks],
     result_extended=True,
     task_track_started=True,
 )
@@ -21,17 +22,17 @@ app = Celery(
 app.conf.update(
     {
         "task_queues": tuple(
-            Queue(task_name, Exchange("tasks_exchange"), routing_key=task_name)
-            for task_name in enabled_task_names
+            Queue(task, Exchange("tasks_exchange"), routing_key=task)
+            for task in loaded_tasks
         ),
         "task_default_exchange": "tasks_exchange",
         "task_default_exchange_type": "direct",
         "task_routes": {
-            f"loaded_tasks.{task_name}._{task_name}": {
-                "queue": task_name,
-                "routing_key": task_name,
+            f"loaded_tasks.{task}._{task}": {
+                "queue": task,
+                "routing_key": task,
             }
-            for task_name in enabled_task_names
+            for task in loaded_tasks
         },
         "result_expires": int(os.environ["RESULT_EXPIRES"]),
         "worker_pool": os.environ["WORKER_POOL"],
