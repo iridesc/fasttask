db80b9aad22b855f242275018e0f7739c486707d
zhangziyuan <zhangziyuan@nsfocus.com>
2025-10-29 12:26:13 +0800

    优化了默认提示

--- fasttask/setting.py
+++ fasttask/setting.py
@@ -1,5 +1,7 @@
 # fasttask setting
 project_title = "⚡Fasttask⚡"
 project_summary = "Fasttask Project"
-project_description = "this is a fasttask project!"
+project_description = (
+    "this is a fasttask project! more info： https://github.com/iridesc/fasttask"
+)
 project_version = "ver: 🐕"
